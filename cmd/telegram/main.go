65a6e015ca305cec69df01f8287eee95f0638733
Kirill Zuyeu <kirillzuyeu@gmail.com>
2026-08-16 12:14:56 +0300

    feat: tg clien configuration

--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"github.com/gotd/td/telegram"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client, err := telegram.ClientFromEnvironment(telegram.Options{})
+	if err != nil {
+		log.Fatalf("failed to create telegram client: %v", err)
+	}
+
+	err = client.Run(ctx, func(ctx context.Context) error {
+
+		api := client.API()
+
+		ds, err := api.HelpGetNearestDC(ctx)
+		if err != nil {
+			// will be removed; example code
+			panic(err)
+		}
+
+		log.Println("ds: %w", ds)
+		return nil
+	})
 }
