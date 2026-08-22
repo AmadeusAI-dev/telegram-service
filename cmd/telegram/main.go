67173f89298964681a2ed6e0d66103df968d395d
Kirill Zuyeu <kirillzuyeu@gmail.com>
2026-08-22 20:43:22 +0300

    refactor: initialization and running in App not in main.go

--- cmd/telegram/main.go
+++ cmd/telegram/main.go
@@ -3,52 +3,23 @@ package main
 import (
 	"context"
 	"log"
-	"log/slog"
-	"os"
 
-	"github.com/AmadeusAI-dev/telegram-service/internal/client/handlers"
+	"github.com/AmadeusAI-dev/telegram-service/internal/app"
 	"github.com/AmadeusAI-dev/telegram-service/internal/config"
-	"github.com/TheKiryuKha/pubsub"
-	"github.com/gotd/td/telegram"
-	"github.com/gotd/td/tg"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
+	cfg := config.Load()
 
-	config := config.Load()
-
-	bus, err := pubsub.New(ctx, config.RabbitMq)
+	application, err := app.New(ctx, *cfg)
 	if err != nil {
-		log.Fatalf("failed to create pubsub: %v", err)
+		log.Fatal(err)
 	}
 
-	// @todo: recovery engine
-	d := tg.NewUpdateDispatcher()
-
-	client, err := telegram.ClientFromEnvironment(telegram.Options{
-		UpdateHandler: d,
-	})
-	if err != nil {
-		log.Fatalf("failed to create telegram client: %v", err)
+	if err := application.Run(ctx); err != nil {
+		log.Fatal(err)
 	}
-
-	handlers.DispatchNewMessages(bus, &d)
-
-	err = client.Run(ctx, func(ctx context.Context) error {
-		status, err := client.Auth().Status(ctx)
-		if err != nil {
-			log.Fatalf("failed to get auth status: %v", err)
-		}
-
-		if !status.Authorized {
-			log.Fatalf("auth session is invalid. Please, update session")
-		}
-
-		return telegram.RunUntilCanceled(ctx, client)
-	})
 }
---
+++ internal/app/app.go
@@ -0,0 +1,66 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/AmadeusAI-dev/telegram-service/internal/client/handlers"
+	"github.com/AmadeusAI-dev/telegram-service/internal/config"
+	"github.com/TheKiryuKha/pubsub"
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/tg"
+)
+
+type App struct {
+	pubsub *pubsub.Pubsub
+	client *telegram.Client
+}
+
+func New(ctx context.Context, cfg config.Config) (*App, error) {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	// @todo: recovery engine
+	d := tg.NewUpdateDispatcher()
+
+	client, err := telegram.ClientFromEnvironment(telegram.Options{
+		UpdateHandler: d,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create telegram client: %w", err)
+	}
+
+	bus, err := pubsub.New(ctx, cfg.RabbitMq)
+	if err != nil {
+		return nil, fmt.Errorf("failed to init pubsub: %w", err)
+	}
+
+	handlers.DispatchNewMessages(bus, &d)
+
+	return &App{
+		pubsub: bus,
+		client: client,
+	}, nil
+}
+
+func (a *App) Run(ctx context.Context) error {
+	err := a.client.Run(ctx, func(ctx context.Context) error {
+		status, err := a.client.Auth().Status(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to get auth status: %w", err)
+		}
+
+		if !status.Authorized {
+			return fmt.Errorf("auth session is invalid. Please, update session")
+		}
+
+		return telegram.RunUntilCanceled(ctx, a.client)
+	})
+	if err != nil {
+		return fmt.Errorf("failed to run telegram client: %w", err)
+	}
+
+	return nil
+}
